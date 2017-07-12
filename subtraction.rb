4a9763aa3a4305166893c75cbc1098a228177672
andyhaynes13 <admin@haynescomputerservice.com>
2017-07-12 11:45:40 -0400

    final subtraction

--- subtraction.rb
+++ subtraction.rb
@@ -1,23 +1,10 @@
-x = 1 - 1
-y = 2 - 2
-z = 3 - 3
 
-def add(a,b)
-	puts "subraction #{a}+#{b}"
+def subtract(a,b)
+	puts "subracting #{a} - #{b}"
 	return a - b
 end
 
-answer = x + y
 
+ p subtract(7,6)
+ p subtract(98,33)
 
-p "1 - 1 = #{x}"
-
-p "2 - 2 =#{y}"
-
-p "3 - 3 = #{z}"
-
-p "#{x} - #{y} =  #{answer}"
-
-p subraction(6,7)
-
-p subtraction(34,98)
