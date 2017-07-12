51f360fdaf368cb5f42f9b064c5ae11207c36bcd
andyhaynes13 <admin@haynescomputerservice.com>
2017-07-12 11:35:42 -0400

    initail subtraction

---
+++ subtraction.rb
@@ -0,0 +1,23 @@
+x = 1 - 1
+y = 2 - 2
+z = 3 - 3
+
+def add(a,b)
+	puts "subraction #{a}+#{b}"
+	return a - b
+end
+
+answer = x + y
+
+
+p "1 - 1 = #{x}"
+
+p "2 - 2 =#{y}"
+
+p "3 - 3 = #{z}"
+
+p "#{x} - #{y} =  #{answer}"
+
+p subraction(6,7)
+
+p subtraction(34,98)
